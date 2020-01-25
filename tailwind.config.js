5f988fafc5eb6e80dc36a81a0d90bbf48ec1c866
shaho <toofani@gmail.com>
2020-01-25 22:37:13 +0330

    Add tailwindcss package

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ["Work Sans", "sans-serif"],
+      mono: ["Fira Code", "monospace"],
+    },
+    extend: {},
+  },
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "active"],
+  },
+  plugins: [],
+};
